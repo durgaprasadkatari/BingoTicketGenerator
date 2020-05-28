315236db85894264e440ea68198f901ae737d129
Durgaprasad Katari <Durgaprasad.Katari@nhg.org.uk>
2020-05-28 16:22:00 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TambolaTicketGenerator;
+
+namespace TambolaGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] ticket = new int[3, 9];
+            TicketGenerator.GenerateTicket(ref ticket);
+            TicketGenerator.PrintTicket(ticket);
+            Console.ReadLine();
+        }
+    }
+}
